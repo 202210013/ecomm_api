3205b3859c95e2f8cd1ff2f63665aa2377105544
Garnnn <202210013@gordoncollege.edu.ph>
2026-04-01 12:03:57 +0800

    clean

--- node-api/src/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const app = require("./app");
-
-const port = Number(process.env.PORT || 8080);
-
-app.listen(port, () => {
-  console.log(`Node API running on port ${port}`);
-});
